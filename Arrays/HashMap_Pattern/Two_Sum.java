e9403a38bb19dba37e14c868497e3067633c5122
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-06-22 09:37:41 +0530

    HashMap Pattern incoming

--- Arrays/HashMap_Pattern/Two_Sum.java
+++ Arrays/HashMap_Pattern/Two_Sum.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       Map<Integer, Integer> map = new HashMap<>();
+       HashMap<Integer, Integer> map = new HashMap<>();
        for(int i=0;i<nums.length;i++)
        {
             int complement = target - nums[i];
