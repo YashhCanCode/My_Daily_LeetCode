f1279080aea28abc8d77b554884b61317e8e29cf
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-06-27 09:39:29 +0530

    SortColors

---
+++ Arrays/SortColors.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class sort_colors {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int i = 0;
+        while(i <= right){
+            if(nums[i] == 0){
+                int temp = nums[i];
+                nums[i] = nums[left];
+                nums[left] = temp;
+                i++;
+                left++;
+            }
+            else if(nums[i] == 2){
+                int temp = nums[i];
+                nums[i] = nums[right];
+                nums[right] = temp;
+                right--;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+}
