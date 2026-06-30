19d8d11269b4664ba76aea817e12969ef162b646
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-06-30 11:39:39 +0530

    Binary Search

---
+++ BinarySearch/Peak_Index_On_Mountain.java
@@ -0,0 +1,18 @@
+public class Peak_Index_On_Mountain {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] < arr[mid+1]){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+    return left;
+    }
+}
