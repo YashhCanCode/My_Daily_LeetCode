ee11ac041cd8869a74d718c23c6212b6f071d5d6
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-02 19:09:30 +0530

    LinkedList

---
+++ Linked_List/Middle_of_the_LL.java
@@ -0,0 +1,23 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode mid = new Solution().middleNode(head);
+        System.out.println(mid.val);
+    }
+}
