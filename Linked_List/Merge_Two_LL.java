cde64901026e6832fe9586e09fb70bce1d710926
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-02 19:35:06 +0530

    LinkedList

---
+++ Linked_List/Merge_Two_LL.java
@@ -0,0 +1,31 @@
+class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       ListNode returnNode = new ListNode(-1);
+       ListNode head = returnNode;
+
+       while(list1!=null && list2!=null){
+
+            if(list1.val <= list2.val){
+                head.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                head.next = list2;
+                list2 = list2.next;
+            }
+        head = head.next;
+       }
+
+       if(list1!=null){
+            head.next = list1;
+       }
+       else if(list2!=null){
+            head.next = list2;
+       }
+
+    return returnNode.next;
+
+    
+
+    }
+}
