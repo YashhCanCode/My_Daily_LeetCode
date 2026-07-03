d7730a34685f7fa5a0a872906997cc639818e13f
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-03 16:37:28 +0530

    LinkedList-2

---
+++ Linked_List/Palindrome_Linked_List.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+class palindrome_linked_list {
+    public boolean isPalindrome(ListNode head) {
+        
+        Stack<Integer> stack = new Stack<>();
+        ListNode curr = head;
+        while(curr!=null){
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr!=null){
+            if(curr.val!=stack.pop()){
+                return false;
+            }
+        curr = curr.next;
+        }
+    return true;
+    }
+}
