f865387fe325b760f4705072fe6863a7d865c528
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-08 22:41:42 +0530

    Island baby

--- Graph/Number_of_islands.java
+++ Graph/Number_of_islands.java
@@ -1,4 +1,4 @@
-public class Number_of_islands {
+public class islands {
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length == 0){
             return 0;
