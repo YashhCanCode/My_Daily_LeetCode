c5823c2606cc46eb4d4f6544333de0e27da97a61
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-10 18:32:17 +0530

    ..

--- Depth_First_Search/Number_of_islands.java
+++ Depth_First_Search/Number_of_islands.java
@@ -1,4 +1,4 @@
-class number_of_islands {
+class Solution {
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length==0) return 0;
         int count = 0;
