8c1359ed83ffebeb503af18e6a55e973bba374a1
YashhCanCode <yashwanthkonnuru@gmail.com>
2026-07-15 11:17:31 +0530

    Highest score

---
+++ Graph/node_with_highest_Score.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        long[] scores = new long[edges.length];
+        for(int i = 0; i < edges.length; i++){
+            scores[edges[i]]+=i;
+        }
+        int answer = 0;
+        for(int i = 0; i<scores.length; i++){
+            if(scores[i] > scores[answer]){
+                answer = i;
+            }
+        }
+    return answer;
+    }
+}
